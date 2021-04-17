c692f63363087f0e14b0df39566fc82960d2fedc
vedika28 <66419325+vedika28@users.noreply.github.com>
2021-04-17 11:47:13 +0530

    Add files via upload

---
+++ components/AppStackNavigator.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "../screens/HomeScreen";
+import ReceiverDetails from "../screens/ReceiverDetails";
+import NotificationsScreen from "../screens/NotificationsScreen";
+
+export const AppStackNavigator = createStackNavigator(
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+    ReceiverDetails: {
+      screen: ReceiverDetails,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+  },
+  {
+    initialRouteName: "HomeScreen",
+  }
+);
